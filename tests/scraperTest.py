0268882386ca98c43dcc53d4f9f729a668ff031f
unknown <mze5111@gmail.com>
2019-01-08 12:36:47 -0500

    Added testing for grabbing 1 episode

--- tests/scraperTest.py
+++ tests/scraperTest.py
@@ -1,15 +1,30 @@
 import sys
 import os
+from random import randint
 sys.path.append(os.getcwd()+'\\src')
 
 from CrunchyWebScraper import CrunchyWebScraper
 
 print("Grab Goblin Links")
-eps = CrunchyWebScraper.scrapeUrlForLinks("https://www.crunchyroll.com/goblin-slayer")
+eps = CrunchyWebScraper.scrapeUrlForEpisodeLinks("https://www.crunchyroll.com/goblin-slayer")
 for ep in eps:
 	print(ep)
 
 print("\nGrab One Piece Links")
-eps = CrunchyWebScraper.scrapeUrlForLinks("https://www.crunchyroll.com/one-piece")
+eps = CrunchyWebScraper.scrapeUrlForEpisodeLinks("https://www.crunchyroll.com/one-piece")
 for ep in eps:
 	print(ep)
+
+print("\nGrab Goblin Ep 6")
+ep = CrunchyWebScraper.getEpisodeLink("https://www.crunchyroll.com/goblin-slayer", 6)
+print(ep)
+
+print("\nGrab 5 random one-piece eps")
+for i in range(5):
+	reqEp = randint(0,1000)
+	print("Grabbing episode {0}".format(reqEp))
+	ep = CrunchyWebScraper.getEpisodeLink("https://www.crunchyroll.com/one-piece", reqEp)
+	if ep:
+		print("\t"+ep)
+	else:
+		print("\tNo episode {0}".format(reqEp))
