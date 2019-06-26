1e64b7c45c7627526347b6454b37d37f6c2c32e8
Matthew Esch <mze@gmail.com>
2019-06-26 10:58:14 -0400

    Framework for changes adding seasons and fixing alias bug.

--- src/discordMain.py
+++ src/discordMain.py
@@ -42,14 +42,19 @@ async def on_message(message):
 
     elif message.content.startswith('!get+'):
         await getCurrEpAndIncriment(message)
+
+    elif message.content.startswith('!set'):
+        await setCurrEp(message)
+
+    elif message.content.startswith('!home'):
+        await getHomeURL(message)
+
     elif message.content.startswith('!exit'):
         await client.logout()
-    else:
-        await client.send_message(message.channel, message.content)
 
 async def help(message):
     helpMsg = 'Hello {0.author.mention}, I am Amadeus!'.format(message)
-    helpMsg += '\nI exist to facilitate anime. Please kill me.\n'
+    helpMsg += '\nI exist to facilitate anime. 大　やばい\n'
 
     stackMsg = '\nTo get started, take a look at the stack with "!stack"'
     stackMsg += '\nThe stack shows all the currently watched anime and the next episode to watch.\n'
@@ -59,20 +64,23 @@ async def help(message):
 
 async def addAnimeToStack(message):
     words = message.content.split()
+
     if len(words) < 2:
         errMsg = 'Please enter the form of: "!stack+ www.url-to-anime-home.com" "optional Alias".'
         await client.send_message(message.channel, errMsg)
         return
+
     url = words[1]
+
     if validators.url(url):
-        animeNameDirty = url.split("/")[-1]
-        animeNameLower = animeNameDirty.replace('-',' ').lower()
-        animeNameClean = " ".join(list(map(lambda x: x.capitalize(), animeNameLower.split())))
+        animeNameClean = cleanAnimeName(url.split("/")[-1])
         amadeusDriver.addUrl(animeNameClean, url)
         amadeusDriver.addStack(animeNameClean)
+        amadeusDriver.setSeason(animeNameClean, "1")
     else:
         errMsg = 'Please confirm you have entered a valid url address.'
         await client.send_message(message.channel, errMsg)
+
     potentialAlias = " ".join(words[2:])
     if potentialAlias:
         amadeusDriver.addAlias(animeNameClean, potentialAlias)
@@ -92,15 +100,14 @@ async def printAlias(message):
 async def addAlias(message):
     words = message.content.split()
     if len(words) < 3:
-        errMsg = 'Please enter the form of: "!alias+ anime-stack-name newAlias".'
+        errMsg = 'Please enter the form of: "!alias+ anime-stack-name newAlias"\nOr:\n"!alias+ currAlias newAlias".'
         await client.send_message(message.channel, errMsg)
         return
-    alias = "".join(words[2:])
-    animeNameLower = words[1].replace('-',' ').lower()
-    animeNameClean = " ".join(list(map(lambda x: x.capitalize(), animeNameLower.split())))
+    newAlias = "".join(words[2:])
+    animeNameClean = cleanAnimeName(words[1])
     if animeNameClean in amadeusDriver.getStack():
-        amadeusDriver.addAlias(animeNameClean, alias)
-        succMsg = 'Added "{0}" as an alias for "{1}".'.format(alias, animeNameClean)
+        amadeusDriver.addAlias(animeNameClean, newAlias)
+        succMsg = 'Added "{0}" as an alias for "{1}".'.format(newAlias, animeNameClean)
         await client.send_message(message.channel, succMsg)
     else:
         errMsg = 'Please confirm you have entered a valid anime name.'
@@ -111,10 +118,15 @@ async def getCurrEpAndIncriment(message):
     if len(words) < 2:
         errMsg = 'Please enter the form of: "!get+ animeTitle/animeAlias".'
         await client.send_message(message.channel, errMsg)
+
     key = "".join(words[1:])
+    print("Entered key:", key)
     trueKey = amadeusDriver.getTitleFromKey(key)
+    print("True key:", trueKey)
+
     if trueKey:
         currEpNum = amadeusDriver.getCurrEpNumber(trueKey)
+        print("Curr Ep:", currEpNum)
         currEpLink = amadeusDriver.getEpisodeFromTitle(trueKey, currEpNum)
         if currEpLink:
             embedEpisode = discord.Embed(url=currEpLink, title = "Webscrap me from the link trasher", description = "Oh No! See above!", color = 16175669)
@@ -130,5 +142,33 @@ async def getCurrEpAndIncriment(message):
     else:
         errMsg = '"{0}" not found in stack or alias list, please confirm the anime and try again.'.format(key)
         await client.send_message(message.channel, errMsg)
+
+async def setCurrEp(message):
+    words = message.content.split()
+    if len(words) < 3:
+        errMsg = 'Please enter the form of: "!set animeTitle/animeAlias epNum".'
+        await client.send_message(message.channel, errMsg)
+    ep = words[-1]
+    try:
+        int(ep)
+        isValidEp = True
+    except:
+        isValidEp = False
+
+    if isValidEp:
+        key = "".join(words[1:-1])
+        trueKey = amadeusDriver.getTitleFromKey(key)
+        amadeusDriver.setStack(trueKey, ep)
+        succMsg = 'Updated stack of {0} to episode {1}'.format(trueKey,ep)
+        await client.send_message(message.channel, succMsg)
+    else:
+        errMsg = 'Please ensure "{0}" is a valid number'.format(ep)
+        await client.send_message(message.channel, errMsg)
+
+def cleanAnimeName(dirtyName):
+    animeNameLower = dirtyName.replace('-',' ').lower()
+    animeNameClean = " ".join(list(map(lambda x: x.capitalize(), animeNameLower.split())))
+    return(animeNameClean)
+
 
 client.run(TokenDistributer.getToken())
--- tests/scraperTest.py
+++ tests/scraperTest.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from random import randint
+
 sys.path.append(os.getcwd()+'\\src')
 
 from CrunchyWebScraper import CrunchyWebScraper
@@ -16,15 +17,20 @@ for ep in eps:
 	print(ep)
 
 print("\nGrab Goblin Ep 6")
-ep = CrunchyWebScraper.getEpisodeLink("https://www.crunchyroll.com/goblin-slayer", 6)
+ep = CrunchyWebScraper.getEpisodeLink("https://www.crunchyroll.com/goblin-slayer", "6")
 print(ep)
 
 print("\nGrab 5 random one-piece eps")
 for i in range(5):
-	reqEp = randint(0,1000)
-	print("Grabbing episode {0}".format(reqEp))
-	ep = CrunchyWebScraper.getEpisodeLink("https://www.crunchyroll.com/one-piece", reqEp)
-	if ep:
-		print("\t"+ep)
-	else:
-		print("\tNo episode {0}".format(reqEp))
+    reqEp = randint(0,1000)
+    print("Grabbing episode {0}".format(reqEp))
+    ep = CrunchyWebScraper.getEpisodeLink("https://www.crunchyroll.com/one-piece", str(reqEp))
+    if ep:
+        print("\t"+ep)
+    else:
+        print("\tNo episode {0}".format(reqEp))
+
+print("\nJoJo's Links")
+eps = CrunchyWebScraper.scrapeUrlForEpisodeLinks("https://www.crunchyroll.com/jojos-bizarre-adventure")
+for ep in eps:
+	print(ep)
