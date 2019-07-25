3320a2d17520783c114df0056bfafdcd52746dae
Matthew Esch <mze@gmail.com>
2019-07-25 14:41:17 -0400

    Modified DictionaryStorage to use pickle. Is now writing the actual python objects instead of strings.

--- amadeus/DictionaryStorage.py
+++ amadeus/DictionaryStorage.py
@@ -1,21 +1,21 @@
 import sys
 import os.path
-import json
+import pickle
 
-# Add ability for multiple data stores (e.g. john_tom, john_kyle)
+#todo Add ability for multiple data stores (e.g. john_tom, john_kyle)
 class DictionaryStorage():
     def __init__(self, filename, data_dir = None):
         self.data = dict()
         if not data_dir:
             data_dir = os.path.abspath(os.path.join(
                 os.path.dirname(__file__), "..", "data"))
-        self.data_filepath = os.path.join(data_dir, filename + ".json")
+        self.data_filepath = os.path.join(data_dir, filename + ".p")
 
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
 
         if os.path.exists(self.data_filepath):
-            self.loadJson()
+            self.loadSerial()
 
     def __str__(self):
         string = "{\n"
@@ -25,24 +25,30 @@ class DictionaryStorage():
         return string
 
     def __getitem__(self, key):
-        return self.data[str(key)]
+        return self.data[key]
 
     def __setitem__(self, key, value):
-        self.data[str(key)] = str(value)
-        self.writeJson()
+        self.data[key] = value
+        self.writeSerial()
+
+    def __iter__(self):
+        return iter(list(self.data.keys()))
 
     def __delitem__(self, key):
         char_key = str(key)
         del self.data[char_key]
-        self.writeJson()
+        self.writeSerial()
 
     def __contains__(self, key):
         return bool(key in self.data) 
 
-    def loadJson(self):
-        with open(self.data_filepath, "r") as fileIO:
-            self.data = json.load(fileIO)
+    def loadSerial(self):
+        with open(self.data_filepath, "rb") as fileIO:
+            self.data = pickle.load(fileIO)
 
-    def writeJson(self):
-        with open(self.data_filepath, "w") as fileIO:
-            json.dump(self.data, fileIO)
+    def writeSerial(self):
+        with open(self.data_filepath, "wb") as fileIO:
+            pickle.dump(self.data, fileIO)
+
+    def keys(self):
+        return(self.data.keys())
--- tests/DictionaryStorage_test.py
+++ tests/DictionaryStorage_test.py
@@ -1,13 +1,25 @@
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 
 from amadeus.DictionaryStorage import DictionaryStorage
 
+
 @pytest.fixture(scope="function")
 def unique_dictionary_storage(tmpdir):
     return DictionaryStorage("testfilename", tmpdir.strpath)
+
+
+@pytest.fixture(scope="function")
+def filled_unique_dictionary_storage(tmpdir):
+    ds = DictionaryStorage("testfilename", tmpdir.strpath)
+    ds[11] = "fishy"
+    ds["dogs"] = 1
+    ds[(1, 2)] = [1, 2, 3]
+    return ds
+
 
 class TestDictionaryStorage():
     @pytest.mark.parametrize("key, value", [
@@ -16,7 +28,7 @@ class TestDictionaryStorage():
         ("dogs", 4),
         ("dogs", "cats")
     ])
-    def test_set(self, unique_dictionary_storage, key, value): 
+    def test_set(self, unique_dictionary_storage, key, value):
         unique_dictionary_storage[key] = value
 
     @pytest.mark.parametrize("key, value", [
@@ -25,10 +37,64 @@ class TestDictionaryStorage():
         ("dogs", 4),
         ("dogs", "cats")
     ])
-    def test_get_1(self, unique_dictionary_storage, key, value): 
+    def test_get_1(self, unique_dictionary_storage, key, value):
         unique_dictionary_storage[key] = value
-        assert unique_dictionary_storage[key] == str(value)
+        assert unique_dictionary_storage[key] == value
 
-    def test_get_2(self, unique_dictionary_storage): 
+    def test_get_2(self, unique_dictionary_storage):
         with pytest.raises(KeyError):
-        	assert unique_dictionary_storage["datathatdoesntexist"] == ""
+            assert unique_dictionary_storage["datathatdoesntexist"] == ""
+
+    def test_iter(self, filled_unique_dictionary_storage):
+        for i in filled_unique_dictionary_storage:
+            print(i)
+
+    # TODO
+    @pytest.mark.skip("Not Implemented")
+    def test_get_keys(self, unique_dictionary_storage):
+        pass
+
+    @pytest.mark.parametrize("key, value", [
+        (11, "fishy"),
+        (10, 7),
+        (1, [1, 2, 3])
+    ])
+    def test_writing_reading_keys_int(self, unique_dictionary_storage, key, value):
+        unique_dictionary_storage[key] = value
+        assert (key in unique_dictionary_storage)
+
+    @pytest.mark.parametrize("key, value", [
+        ("one", "fishy"),
+        ("two", 7),
+        ("3", [1, 2, 3])
+    ])
+    def test_writing_reading_keys_string(self, unique_dictionary_storage, key, value):
+        unique_dictionary_storage[key] = value
+        assert (key in unique_dictionary_storage)
+
+    @pytest.mark.parametrize("key, value", [
+        ("one", 1),
+        ("two", 2),
+        ("3", 3)
+    ])
+    def test_writing_reading_values_int(self, unique_dictionary_storage, key, value):
+        unique_dictionary_storage[key] = value
+        assert (unique_dictionary_storage[key] == value)
+
+    @pytest.mark.parametrize("key, value", [
+        ("one", "1"),
+        (2, "cat"),
+        ((2, 3), "BigBoiOnly")
+    ])
+    def test_writing_reading_values_string(self, unique_dictionary_storage, key, value):
+        unique_dictionary_storage[key] = value
+        assert (unique_dictionary_storage[key] == value)
+
+    @pytest.mark.parametrize("key, value", [
+        ("one", [1, 2, 3]),
+        (2, ["a", "b", "c"]),
+        ((2, 3), ["BigBoiOnly", 43, (1, 2)])
+    ])
+    def test_writing_reading_values_list(self, unique_dictionary_storage, key, value):
+        unique_dictionary_storage[key] = value
+        assert (unique_dictionary_storage[key] == value)
